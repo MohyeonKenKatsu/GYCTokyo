e99544c9ea6e13c42e6d419ebc44e3f52fa8a217
chiihyun <lieear24@gmail.com>
2025-01-10 09:39:53 +0900

    Feat: DTO개발

--- GYCTokyo/src/main/java/BeansHealth/HealthDTO.java
+++ GYCTokyo/src/main/java/BeansHealth/HealthDTO.java
@@ -1,24 +1,161 @@
+// #################################################################################################
+// UsersDTO.java - USERS 테이블 정보 전달용 DTO
+// #################################################################################################
+// ═════════════════════════════════════════════════════════════════════════════════════════
+// 외부모듈 영역
+// ═════════════════════════════════════════════════════════════════════════════════════════
 package BeansHealth;
 
-public class HealthDTO {
+import Common.ExceptionMgr;
 
-   
-   /** group_id         : Beans 그룹 아이디 필드 (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private Integer group_id = 0;
-   /** user_id            : Beans 유저 아이디 필드 (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private Integer user_id = 0;
-   /** title            : Beans 모임만들기 모달창 제목 (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private String title = null;
-   /** start_date         : Beans 모임만들기 모달창 시작날짜 (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private String start_date = null;
-   /** finish_date         : Beans 모임만들기 모달창 종료날짜 (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private String finish_date = null;
-   /** activity_date      : Beans 모임만들기 모달창 활동날짜 (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private String activity_date = null;
-   /** number_limit      : Beans 모임만들기 모달창 모집인원 수(최대) (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private String number_limit = null;
-   /** content            : Beans 모임만들기 모달창 상세내용 (JSP 입력 객체와 1:1 대응, 소문자로 시작)*/
-   private String content = null;
-   
-   
+// ═════════════════════════════════════════════════════════════════════════════════════════
+// 사용자정의 클래스 영역
+// ═════════════════════════════════════════════════════════════════════════════════════════
+/***********************************************************************
+ * UsersDTO	: 기본 DTO 클래스<br>
+ * Inheritance	: None
+ ***********************************************************************/
+public class HealthDTO
+{
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 전역상수 관리 - 필수영역
+	// —————————————————————————————————————————————————————————————————————————————————————
+	
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 전역변수 관리 - 필수영역(정적변수)
+	// —————————————————————————————————————————————————————————————————————————————————————
+    
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 전역변수 관리 - 필수영역(인스턴스변수)
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 건강기록 날짜
+	private String health_date 				= null;
+	
+	// 사용자 아이디 
+	private Integer user_id 				= null;
+	
+	// 목표 음수량
+	private String 	goal_water	 			= null;
+	
+	// 달성 음수량
+	private String 	achieved_water 			= null;
+	
+	// 운동 종류
+	private String 	type_exercise 			= null;
+	
+	// 목표 운동기록
+	private String goal_exercise	 		= null;
+	
+	// 달성 운동기록
+	private String achieved_exercise 		= null;
+	
+	// 취침시간
+	private String 	bedtime		 			= null;
+	
+	// 기상시간
+	private String waketime			 		= null;
+
+	// —————————————————————————————————————————————————————————————————————————————————————
+    // 생성자 관리 - 필수영역(인스턴스함수)
+	// —————————————————————————————————————————————————————————————————————————————————————
+	/***********************************************************************
+	 * UsersDTO()	: 생성자
+	 * @param void	: None
+	 ***********************************************************************/
+	public HealthDTO()
+	{
+		try
+		{
+			// -----------------------------------------------------------------------------
+			// 기타 초기화 작업 관리
+			// -----------------------------------------------------------------------------
+			ExceptionMgr.SetMode(ExceptionMgr.RUN_MODE.DEBUG);
+			// -----------------------------------------------------------------------------
+		}
+		catch (Exception Ex)
+		{
+			ExceptionMgr.DisplayException(Ex);		// 예외처리(콘솔)
+		}
+    }
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 전역함수 관리 - 필수영역(정적함수)
+	// —————————————————————————————————————————————————————————————————————————————————————
+	
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 전역함수 관리 - 필수영역(인스턴스함수)
+	// —————————————————————————————————————————————————————————————————————————————————————
+	public String getHealth_date() {
+		return health_date;
+	}
+
+	public void setHealth_date(String health_date) {
+		this.health_date = health_date;
+	}
+
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getGoal_water() {
+		return goal_water;
+	}
+
+	public void setGoal_water(String goal_water) {
+		this.goal_water = goal_water;
+	}
+
+	public String getAchieved_water() {
+		return achieved_water;
+	}
+
+	public void setAchieved_water(String achieved_water) {
+		this.achieved_water = achieved_water;
+	}
+
+	public String getType_exercise() {
+		return type_exercise;
+	}
+
+	public void setType_exercise(String type_exercise) {
+		this.type_exercise = type_exercise;
+	}
+
+	public String getGoal_exercise() {
+		return goal_exercise;
+	}
+
+	public void setGoal_exercise(String goal_exercise) {
+		this.goal_exercise = goal_exercise;
+	}
+
+	public String getAchieved_exercise() {
+		return achieved_exercise;
+	}
+
+	public void setAchieved_exercise(String achieved_exercise) {
+		this.achieved_exercise = achieved_exercise;
+	}
+
+	public String getBedtime() {
+		return bedtime;
+	}
+
+	public void setBedtime(String bedtime) {
+		this.bedtime = bedtime;
+	}
+
+	public String getWaketime() {
+		return waketime;
+	}
+
+	public void setWaketime(String waketime) {
+		this.waketime = waketime;
+	}
 }
+// #################################################################################################
+// <END>
+// #################################################################################################
